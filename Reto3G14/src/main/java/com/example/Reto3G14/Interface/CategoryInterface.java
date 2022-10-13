8caf8eb71b5730d0d630a2a6b6575dd42c41f023
JhoanSTalero13 <jhoan.talero@unigermana.edu.co>
2022-10-13 10:18:19 -0500

    prueba 3

---
+++ Reto3G14/src/main/java/com/example/Reto3G14/Interface/CategoryInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.Reto3G14.Interface;
+
+import com.example.Reto3G14.Modelo.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author MG1323
+ */
+public interface CategoryInterface extends CrudRepository <Category,Integer >{
+    
+    
+}
---
+++ Reto3G14/src/main/java/com/example/Reto3G14/Reto3G14Application.java
@@ -0,0 +1,14 @@
+package com.example.Reto3G14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3G14Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Reto3G14Application.class, args);
+	}
+
+}
